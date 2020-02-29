2c7e6a703675488fbf81719aaba7c78ac5fea9aa
Jeff Mony <jeffmony@163.com>
2020-03-01 00:25:52 +0800

    1.本地完善so 自动化编译脚本
    Signed-off-by: Jeff Mony <jeffmony@163.com>

---
+++ build_jeffmony_so.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+export PROJECT_PATH=$(pwd)
+
+echo ${PROJECT_PATH}
+
+cd android/contrib/
+
+# compile openssl
+./compile-openssl.sh clean
+./compile-openssl.sh armv5
+./compile-openssl.sh armv7a
+./compile-openssl.sh arm64
+
+# compile ffmpeg
+./compile-ffmpeg.sh clean
+./compile-ffmpeg.sh armv5
+./compile-ffmpeg.sh armv7a
+./compile-ffmpeg.sh arm64
+
+cd ..
+
+# compile ijkplayer
+./compile-ijk.sh clean
+./compile-ijk.sh armv5
+./compile-ijk.sh armv7a 
+./compile-ijk.sh arm64
+
+cd ..
+rm -rf jeffmony_files/*
+cp -rf ${PROJECT_PATH}/android/ijkplayer/ijkplayer-armv5/src/main/libs/ ./jeffmony_files/
+cp -rf ${PROJECT_PATH}/android/ijkplayer/ijkplayer-armv7a/src/main/libs/ ./jeffmony_files/
+cp -rf ${PROJECT_PATH}/android/ijkplayer/ijkplayer-arm64/src/main/libs/ ./jeffmony_files/
